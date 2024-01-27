5adb932b922a58cb9bd984f61bb6e4e028c99770
LordBAP <lordbap@gmail.com>
2024-01-27 01:09:41 -0500

    Setup base
    Added rotation into prefabs, added scenes for levels

--- Assets/Scripts/FunnyEventer.cs
+++ Assets/Scripts/FunnyEventer.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class FunnyEventer : MonoBehaviour
 {
     public GameObject MyFunny;
-
-
+ 
     void OnMouseDown()
     {
         //enable the drop and set in motion
         MyFunny.SetActive(true);
         Debug.Log("clicked");
+
+        //change image to post clicked
+        
     }
     // Start is called before the first frame update
 }
--- Assets/Scripts/FunnyHitter.cs
+++ Assets/Scripts/FunnyHitter.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class FunnyHitter : MonoBehaviour
 {
-
+    
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
-        Debug.Log("HIT");
+        
         if (collision.gameObject.tag == "Player")
         {
             Debug.Log("HITsss");
+            //call player object to stop moving 
+            Destroy(this.gameObject);
+        }else if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("Grounded");
             Destroy(this.gameObject);
         }
         
+    }
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
     }
 }
